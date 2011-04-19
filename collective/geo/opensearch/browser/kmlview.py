1f163126310526f9b33ffb94cf806a7b7cc04da7
nan010 <nan010@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-04-19 14:52:47 +0000

    add kml view for searches

---
+++ collective/geo/opensearch/browser/kmlview.py
@@ -0,0 +1,25 @@
+from zope.interface import implements, Interface
+from plone.memoize import view
+
+from collective.geo.kml.browser.kmldocument import KMLBaseDocument, BrainPlacemark
+
+from collective.opensearch.browser import search
+
+class IKMLView(Interface):
+    """
+    Search Kml view interface
+    """
+
+
+class KMLView(KMLBaseDocument):
+    """
+    FlexiTopicKml browser view
+    """
+    implements(IKMLView)
+
+    @property
+    @view.memoize
+    def features(self):
+        search_results = search.get_results(self.context, self.request)
+        for brain in search_results:
+            yield BrainPlacemark(brain, self.request, self)
